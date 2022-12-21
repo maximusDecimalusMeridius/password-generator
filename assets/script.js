7b684734899c0c34ad3909fddcd7f5796cbb62da
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2022-12-21 15:14:39 -0800

    Cleared up bugs from repeat session in browser by reinitialzing object property string values where I store updates

--- assets/script.js
+++ assets/script.js
@@ -2,28 +2,28 @@
 document.getElementById("generate").addEventListener("click", OnClick);
 
 // pwBuild object
-var pwBuild = {
+let pwBuild = {
     passwordLength: 0,
-    availableCharacters: "",
+    availableChars: "",
     guaranteedChars: "",
     newPassword: "",
     lowercase:{
-        use: 0,
+        use: false,
         values: "abcdefghijklmnopqrstuvwxyz",
         ID: "lowercase letters"
     },
     uppercase:{
-        use: 0,
+        use: false,
         values: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
         ID: "uppercase letters"
     },
     numbers:{
-        use: 0,
+        use: false,
         values: "0123456789",
         ID: "numbers"
     },
     specialChars:{
-        use: 0,
+        use: false,
         values: "~!@#$%^&*(){}[]:;<>?/-_+=|",
         ID: "special characters"
     }
@@ -32,18 +32,25 @@ var pwBuild = {
 //Prompt user for an input, store value in variable
 //If smaller than 8 or larger than 128, prompt again
 function OnClick(){
-    
+    // Initialize guaranteed char string
+    pwBuild.guaranteedChars = "";
+    pwBuild.availableChars = "";
+    pwBuild.newPassword = "";
+
+    console.log("Guaranteed: " + pwBuild.guaranteedChars)
+    console.log("Available: " + pwBuild.availableChars)
+    console.log("New Password: " + pwBuild.newPassword)
+
     pwBuild.passwordLength = prompt("Enter your desired password length between 8 and 128")
     if(pwBuild.passwordLength == null){
         return;
     }
-    while(!(pwBuild.passwordLength >= 8) && !(pwBuild.passwordLength <= 128)){
+    while(!(pwBuild.passwordLength >= 8) || !(pwBuild.passwordLength <= 128)){
         pwBuild.passwordLength = prompt("Enter your desired password length between 8 and 128");
         if(pwBuild.passwordLength == null){
             return;
         }
     }
-
 
     // Call InitializeChoice which will prompt user for requirement prefs and act accordingly
     InitializeChoice(pwBuild.lowercase);
@@ -51,13 +58,14 @@ function OnClick(){
     InitializeChoice(pwBuild.specialChars);
     InitializeChoice(pwBuild.numbers);
 
-    if(pwBuild.lowercase.use == false && pwBuild.uppercase.use == false && pwBuild.numbers.use == false && !pwBuild.specialChars.use.NaN){
+    if(pwBuild.lowercase.use == false && pwBuild.uppercase.use == false && pwBuild.numbers.use == false && pwBuild.specialChars.use == false){
         document.getElementById("password").value = "Come onnnnnnnn, you have to pick at least ONE!";
     } else {
         GeneratePassword(pwBuild.passwordLength);
         JumblePass();
         document.getElementById("password").value = pwBuild.newPassword;
     }
+
 }
 
 // Generate a new password by concatenating pwBuild.newPassword
@@ -69,39 +77,42 @@ function GeneratePassword(remainingLetters) {
         return pwBuild.newPassword;
     }
 
-    pwBuild.newPassword += pwBuild.availableCharacters.charAt(Math.floor(Math.random() * pwBuild.availableCharacters.length));
+    pwBuild.newPassword += pwBuild.availableChars.charAt(Math.floor(Math.random() * pwBuild.availableChars.length));
     GeneratePassword(remainingLetters - 1);
+
 }
 
 // Accept user preferences through confirm() dialog pop-ups.  If user selects a characteristic,
-// it guarantees at least two of those values will be added to the availableCharacters string 
+// it guarantees at least two of those values will be added to the availableChars string 
 function InitializeChoice(choice){
-    console.log(choice);
     
     if(choice.use = confirm(`Would you like to include ${choice.ID}?
 Click OK for 'Yes'`)){
-        console.log(choice.use);     
-        pwBuild.availableCharacters += choice.values;
+        console.log(choice.use);
+        pwBuild.availableChars += choice.values;
         for(let i = 0; i < 2; i++){
+            console.log("Adding stuff");
             pwBuild.guaranteedChars += choice.values.charAt(Math.floor(Math.random() * choice.values.length));
             pwBuild.passwordLength -= 1;
         }
-    }
+    } else {console.log(choice.use)}
 }
 
 // Jumble the new password
 function JumblePass(){
     let tempArray = [];
+    console.log("tempArray2 =" + tempArray);
     let randomizedPassword = "";
     pwBuild.newPassword += pwBuild.guaranteedChars;
     
     // for loop creating a temp array from the newPassword string value in pwBuild,
     // so we can manipulate the data
+
     for(let i = 0; i < pwBuild.newPassword.length; i++){
         tempArray.push(pwBuild.newPassword.charAt(i));
     }
 
-    console.log(tempArray);
+    console.log("tempArray3 = " + tempArray);
 
     // for loop running for (user selected build length) + (length of the string holding guaranteed characters)
     // fatePicker is assigned 0/1
@@ -110,9 +121,21 @@ function JumblePass(){
         if(Math.floor(Math.random() * 2) === 1) {
             randomizedPassword += tempArray.shift();
             console.log("I shifted!");
+            console.log(tempArray);
         } else {
             randomizedPassword += tempArray.pop(); console.log("I POPPED!");
+            console.log(tempArray);
         }
     }
+    // Set new password and re-initialized tempArray
     pwBuild.newPassword = randomizedPassword;
+    console.log("Guaranteed: " + pwBuild.guaranteedChars)
+    console.log("Available: " + pwBuild.availableChars)
+    console.log("New Password: " + pwBuild.newPassword)
+    console.log("tempArray =" + tempArray);
+
+    //Re-initialize array
+    for(index in tempArray){
+        delete tempArray[index];
+    }
 }
